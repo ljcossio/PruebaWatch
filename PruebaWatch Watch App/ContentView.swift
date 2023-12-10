2144e77b155adec65d4418dae648a67e428e2484
ljcossio <79971098+ljcossio@users.noreply.github.com>
2023-12-10 06:52:59 +0100

    Initial Commit

---
+++ PruebaWatch Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PruebaWatch Watch App
+//
+//  Created by Luis Cossío on 10/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PruebaWatch Watch App/PruebaWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaWatchApp.swift
+//  PruebaWatch Watch App
+//
+//  Created by Luis Cossío on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PruebaWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
